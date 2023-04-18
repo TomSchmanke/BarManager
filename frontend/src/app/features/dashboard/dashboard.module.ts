7b08445554b1216951ed99587ec32199c260068e
Jonas Knebel <jonas.knebel.ext@hdi.de>
2023-04-18 08:16:03 +0200

    started working on dashboard

--- frontend/src/app/features/dashboard/dashboard.module.ts
+++ frontend/src/app/features/dashboard/dashboard.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { TableHeaderComponent } from './components/table-header/table-header.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent, TableHeaderComponent],
   imports: [CommonModule, SharedModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
--- frontend/src/app/store/bar/bar.reducers.ts
+++ frontend/src/app/store/bar/bar.reducers.ts
@@ -1,21 +1,29 @@
 import { createReducer, on } from '@ngrx/store';
 import * as BarActions from './bar.actions';
+import { Bar } from '@bar-manager/api';
 
 export const featureKey = 'bar';
 
 export interface IState {
-  bar: any;
+  bar: Bar;
   loading: boolean;
 }
 
 export const initialState: IState = {
-  bar: {},
+  bar: {
+    id: 1,
+    name: 'Hello world',
+    owner: {
+      id: 1,
+      name: 'Init',
+    },
+  },
   loading: false,
 };
 
 export const reducer = createReducer(
   initialState,
-  on(BarActions.loadBar, state => ({ ...state, bar: {}, loading: true })),
+  on(BarActions.loadBar, state => ({ ...state, loading: true })),
   on(BarActions.loadBarSuccess, (state, action) => ({ ...state, bar: action.bar, loading: false })),
-  on(BarActions.loadBarFailure, state => ({ ...state, bar: {}, loading: false }))
+  on(BarActions.loadBarFailure, state => ({ ...state, loading: false }))
 );

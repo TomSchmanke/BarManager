4f5d8c14fb1d03bb78124fb12ee8acae4c479fd6
Laura Schmidt <lauschmidt.ls@gmail.com>
2023-04-28 14:44:01 +0200

    add service scaffolds

---
+++ backend/src/main/kotlin/de/fhswf/barmanager/backend/service/CocktailService.kt
@@ -0,0 +1,8 @@
+package de.fhswf.barmanager.backend.service
+
+import org.springframework.web.context.annotation.ApplicationScope
+
+@ApplicationScope
+class CocktailService {
+
+}
---
+++ backend/src/main/kotlin/de/fhswf/barmanager/backend/service/LoginService.kt
@@ -0,0 +1,7 @@
+package de.fhswf.barmanager.backend.service
+
+import org.springframework.web.context.annotation.ApplicationScope
+
+@ApplicationScope
+class LoginService {
+}

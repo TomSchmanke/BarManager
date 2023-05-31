38ae371be4dfe2c92f404baef7302757139c9ab8
jonas <jonas.knebel@gmx.de>
2023-05-31 18:48:26 +0200

    fixed reset of cocktail in order

--- frontend/src/app/features/orders/components/orders-detail/orders-detail.component.ts
+++ frontend/src/app/features/orders/components/orders-detail/orders-detail.component.ts
@@ -3,7 +3,7 @@ import { Cocktail, Order, UnitOfMeasurement } from '@bar-manager/api';
 import { Store } from '@ngrx/store';
 import { Observable, delay, filter, from, of, switchMap } from 'rxjs';
 import { selectSelectedOrder } from 'src/app/store/orders/orders.selectors';
-import { loadCocktail } from 'src/app/store/recipes/cocktails.actions';
+import { loadCocktail, resetSelectedCocktail } from 'src/app/store/recipes/cocktails.actions';
 import {
   selectSelectedCocktail,
   selectSelectedCocktailsLoadingStatus,
@@ -20,6 +20,7 @@ export class OrdersDetailComponent {
   currentlySelectedCocktail$: Observable<Cocktail | undefined>;
 
   constructor(private store: Store) {
+    this.store.dispatch(resetSelectedCocktail());
     this.loading$ = this.store.select(selectSelectedCocktailsLoadingStatus);
     this.currentlySelectedOrder$ = this.store.select(selectSelectedOrder);
     this.currentlySelectedCocktail$ = this.store.select(selectSelectedCocktail);
